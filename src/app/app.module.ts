c2719a7169a71a1a74ab25c45834e5028f82ca2f
rdz <rdz.carlos@mail.com>
2019-04-23 23:20:11 -0500

    version final 3 hojas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { GaugeChartModule } from 'angular-gauge-chart';
+import { SignaturePadModule } from 'angular2-signaturepad';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SignaturePadModule,
+    GaugeChartModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
